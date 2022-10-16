96e9bdc4861e7e48bf6f0541c324bf19506f42cb
Anas Saeed <saeedanas396@gmail.com>
2022-10-16 06:26:02 -0700

    Added vue template for solana

---
+++ app/vue.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer']
+      })
+    ],
+    resolve: {
+      fallback: {
+        crypto: false,
+        fs: false,
+        assert: false,
+        process: false,
+        util: false,
+        path: false,
+        stream: false,
+      }
+    }
+  }
+})
