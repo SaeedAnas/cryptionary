e563391f68bc06f47f8986d0743593c2b8749f5e
Anas Saeed <saeedanas396@gmail.com>
2022-10-16 06:04:29 -0700

    implemented basic blockchain

--- programs/cryptionary/src/lib.rs
+++ programs/cryptionary/src/lib.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BcLbLvDiJRwsfDb7sPpmUqd3Z7w3i4CFunMyk7me3D2X");
 
 #[program]
 pub mod cryptionary {
@@ -9,7 +11,71 @@ pub mod cryptionary {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn send_post(ctx: Context<SendPost>, correct_guess: String) -> Result<()> {
+        let post: &mut Account<Post> = &mut ctx.accounts.post;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+
+        if correct_guess.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into());
+        }
+
+        post.author = *author.key;
+        post.timestamp = clock.unix_timestamp;
+        post.correct_guess = correct_guess;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Post {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub correct_guess: String,
+    pub num_incorrect: i32,
+    pub is_solved: bool,
+    // TODO: image_hash
+}
+
+// 2. Add some useful constants for sizing propeties.
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const COUNTER_LENGTH: usize = 4;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const BOOL_LENGTH: usize = 1;
+const MAX_GUESS_LENGTH: usize = 50 * 4; // 50 chars max.
+
+// 3. Add a constant on the Tweet account that provides its total size.
+impl Post {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_GUESS_LENGTH // Guess
+        + COUNTER_LENGTH // Guess counter
+        + BOOL_LENGTH; // Is_solved
+}
+
+#[derive(Accounts)]
+pub struct SendPost<'info> {
+    #[account(init, payer = author, space = Post::LEN)]
+    pub post: Account<'info, Post>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+}
--- tests/cryptionary.ts
+++ tests/cryptionary.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Cryptionary } from "../target/types/cryptionary";
+import * as assert from "assert";
 
 describe("cryptionary", () => {
   // Configure the client to use the local cluster.
@@ -9,8 +10,81 @@ describe("cryptionary", () => {
   const program = anchor.workspace.Cryptionary as Program<Cryptionary>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const post = anchor.web3.Keypair.generate();
+
+    await program.rpc.sendPost("Aple", {
+      accounts: {
+        post: post.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [post],
+    });
   });
+
+  it("can send a new post", async () => {
+    const post = anchor.web3.Keypair.generate();
+    await program.rpc.sendPost("Apple", {
+      accounts: {
+        post: post.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [post],
+    });
+
+    const postAccount = await program.account.post.fetch(post.publicKey);
+    console.log(postAccount);
+    assert.equal(
+      postAccount.author.toBase58(),
+      program.provider.wallet.publicKey.toBase58()
+    );
+    assert.equal(postAccount.correctGuess, "Apple");
+  });
+
+  it("can send a new tweet from a different author", async () => {
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(
+      otherUser.publicKey,
+      1000000000
+    );
+    await program.provider.connection.confirmTransaction(signature);
+    const post = anchor.web3.Keypair.generate();
+
+    await program.rpc.sendPost("apple", {
+      accounts: {
+        post: post.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [otherUser, post],
+    });
+
+    const postAccount = await program.account.post.fetch(post.publicKey);
+    assert.equal(postAccount.author.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(postAccount.correctGuess, "apple");
+  });
+
+  it('can fetch all tweets', async () => {
+    const postAccounts = await program.account.post.all()
+    assert.equal(postAccounts.length, 3);
+  })
+
+it('can filter posts by author', async () => {
+  const authorPublicKey = program.provider.wallet.publicKey
+  const postAccounts = await program.account.post.all([
+    {
+      memcmp: {
+        offset: 8,
+        bytes: authorPublicKey.toBase58(),
+      }
+    }
+  ])
+
+    assert.equal(postAccounts.length, 2);
+    assert.ok(postAccounts.every(postAccount => {
+      return postAccount.account.author.toBase58() === authorPublicKey.toBase58()
+    }))
+})
 });
+
